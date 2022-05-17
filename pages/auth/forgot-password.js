6c7ba3a685d54cfeb80ddffeab4c329fe7af0714
Bryan BERGER <bryan@macbook-pro-de-bryan.home>
2022-05-17 16:30:46 +0200

    feat(auth): add error handling and loading on forgot password and reset password pages

--- pages/auth/forgot-password.js
+++ pages/auth/forgot-password.js
@@ -1,17 +1,39 @@
 import axios from 'axios'
 import { Field, Form, Formik } from 'formik'
-import { FiAlertCircle } from "react-icons/fi"
+import { FiAlertCircle, FiSend } from "react-icons/fi"
 import * as Yup from 'yup'
+import Button from '../../components/ui/Button'
+import { useState, useEffect } from 'react'
+import useErrorsTranslator from '../../helpers/errors-translator'
 
 export default function ForgotPasswordPage() {
+
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [emailSent, setEmailSent] = useState(false)
+    const [counter, setCounter] = useState(60)
+
+    const { getTranslatedError } = useErrorsTranslator({locale: 'fr'})
 
     const ForgotPasswordFormSchema = Yup.object().shape({
         email: Yup.string().email('Email invalide').required('Champs requis')
     })
 
+    const startCountDown = (delay) => {
+        setCounter(delay)
+        const timer = setInterval(() => {
+            delay--
+            setCounter(delay)
+            if (delay === 0) {
+                clearInterval(timer)
+                setEmailSent(false)
+            }
+        }, 1000)
+    }
+
     const handleForgotPasswordFormSubmit = async (values) => {
         const { email } = values
-        
+        setLoading(true)
         try {
             const response = await axios.post(`/api/auth/reset-password`, {email}, {
                 withCredentials: true,
@@ -19,18 +41,32 @@ export default function ForgotPasswordPage() {
                     'Content-Type': 'application/json'
                 }
             })
-            return response.data
-        } catch (error) {
-            throw error
+            setLoading(false)
+            setEmailSent(true)
+            startCountDown(60);
+        } catch (err) {
+            setLoading(false)
+            if (err.response && err.response.data && err.response.data.code) {
+                const errorMessage = getTranslatedError(err.response.data.code)
+                setError(errorMessage)
+                return
+            }
+            console.error(error)
         }
     }
 
 
     return(
         <div className='flex justify-center items-center h-full'>
+             {
+                loading &&
+                <div className="absolute inset-0 z-50 bg-gray-50/30 flex items-center justify-center">
+                    <AiOutlineLoading3Quarters className={`text-6xl text-indigo-500 ${loading && 'animate-spin'}`} />
+                </div>
+            }
             <div className="w-11/12 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white drop-shadow rounded-md p-6">
                 <h1 className='text-indigo-500 text-center text-3xl mb-3'>Next-Base</h1>
-                <h2 className='text-gray-500 text-center text-2xl mb-5'>Mot de passe oublié</h2>
+                <h2 className='text-gray-500 text-center text-2xl mb-6'>Mot de passe oublié</h2>
                 <Formik
                     initialValues={{
                         email: ''
@@ -40,15 +76,38 @@ export default function ForgotPasswordPage() {
                 >
                     {({errors, touched}) => (
                         <Form>
-                            <div className="flex flex-col mb-3 text-sm relative">
+                            <div className="flex flex-col mb-5 text-sm relative">
                                 <label htmlFor="forgotPasswordEmailInput" className="text-gray-600 mb-1 ml-1">Adresse email <span className="text-rose-500">*</span></label>
                                 <Field name='email' type="email" className="p-2 rounded-lg border-[0.5px] border-gray-200 bg-gray-50" id="forgotPasswordEmailInput" placeholder="example@example.com" />
                                 {touched.email && errors.email && <span className='ml-2 flex items-center text-rose-500 absolute bottom-2 right-2'><span className='mr-1'>{errors.email}</span><FiAlertCircle /></span>}
                             </div>
-                            <div className="flex flex-col justify-center align-center">
+                            {/* <div className="flex flex-col justify-center align-center">
                                 <button className="px-4 py-2 mb-5 mx-auto bg-gradient-to-r from-indigo-700 to-indigo-500 text-gray-50 rounded-md" type="submit">
                                     Envoyer
                                 </button>
+                            </div> */}
+                            <div className="flex flex-col justify-center items-center text-sm">
+                                {error && <p className='text-sm text-red-500 mb-5'>{error}</p>}
+                                {   !emailSent &&
+                                    <>
+                                        <Button variant={'primary-gradient'} type='submit'>
+                                            <FiSend />
+                                            <span>Envoyer</span>
+                                        </Button>
+                                        <Button variant={'link'} href='/auth/sigin'>
+                                            Retour
+                                        </Button>
+                                    </>
+                                }
+                                {
+                                    emailSent &&
+                                    <>
+                                    <p className='mb-3 text-green-500'>Email envoyé !</p>
+                                        <div className="px-3 py-2 bg-gray-300 rounded-md">
+                                            Renvoyer un email dans {counter} secondes        
+                                        </div>
+                                    </>
+                                }
                             </div>
                         </Form>
                     )}
--- pages/auth/reset-password/[token].js
+++ pages/auth/reset-password/[token].js
@@ -2,11 +2,20 @@ import { useRouter } from "next/router"
 import * as Yup from 'yup'
 import { Formik, Form, Field } from "formik"
 import axios from "axios"
-import { FiAlertCircle } from "react-icons/fi"
+import { FiAlertCircle, FiCheckCircle, FiLogIn, FiSave } from "react-icons/fi"
+import { useState } from 'react'
+import useErrorsTranslator from '../../../helpers/errors-translator'
+import Button from "../../../components/ui/Button"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
 
 export default function ResetPasswordPage() {
 
     const router = useRouter()
+    const [loading, setLoading] = useState(false)
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState(null)
+
+    const { getTranslatedError } = useErrorsTranslator({locale: 'fr'})
 
     const { token } = router.query
 
@@ -17,8 +26,7 @@ export default function ResetPasswordPage() {
 
     const handleResetPasswordFormSubmit = (values) => {
         const { password } = values
-
-        console.log(values)
+        setLoading(true)
 
         axios.put(`/api/auth/reset-password`, {token, password}, {
             withCredentials: true,
@@ -26,46 +34,82 @@ export default function ResetPasswordPage() {
                 'Content-Type': 'application/json'
             }
         }).then(response => {
-            console.log(response)
-            // Navigate to login
+            setSuccess(true)
+            setTimeout(() => {
+                router.replace('/auth/signin')
+            }, 3000)
         }).catch(error => {
-            console.error(error.response.data)
+            if (err.response && err.response.data) {
+                const errorMessage = getTranslatedError(err.response.data.code)
+                return setError(errorMessage)
+            }
+            console.error(error)
+        }).finally(() => {
+            setLoading(false)
         })
     }
     return(
         <div className='flex justify-center items-center h-screen bg-gray-50'>
-            <div className="w-11/12 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white drop-shadow rounded-md p-6">
-                <h1 className='text-indigo-500 text-center text-3xl mb-3'>Next-Base</h1>
-                <h2 className='text-gray-500 text-center text-2xl mb-5'>Nouveau mot de passe</h2>
-                <Formik
-                    initialValues={{
-                        password: '',
-                        confirmPassword: ''
-                    }}
-                    validationSchema={ResetPasswordFormSchema}
-                    onSubmit={handleResetPasswordFormSubmit}
-                >
-                    {({errors, touched}) => (
-                        <Form>
-                            <div className="flex flex-col mb-5 text-sm relative">
-                                <label htmlFor="resetPasswordInput" className="text-gray-600 mb-1 ml-1">Mot de passe <span className="text-rose-500">*</span></label>
-                                <Field name="password" type="password" className="p-2 rounded-lg border-[0.5px] border-gray-200 bg-gray-50" id="resetPasswordInput" placeholder="********" />
-                                {touched.password && errors.password && <span className='ml-2 flex items-center text-rose-500 absolute bottom-2 right-2'><span className='mr-1'>{errors.password}</span><FiAlertCircle /></span>}
-                            </div>
-                            <div className="flex flex-col mb-5 text-sm relative">
-                                <label htmlFor="resetConfirmPasswordInput" className="text-gray-600 mb-1 ml-1">Confirmez le mot de passe <span className="text-rose-500">*</span></label>
-                                <Field name="confirmPassword" type="password" className="p-2 rounded-lg border-[0.5px] border-gray-200 bg-gray-50" id="resetConfirmPasswordInput" placeholder="********" />
-                                {touched.confirmPassword && errors.confirmPassword && <span className='ml-2 flex items-center text-rose-500 absolute bottom-2 right-2'><span className='mr-1'>{errors.confirmPassword}</span><FiAlertCircle /></span>}
-                            </div>
-                            <div className="flex flex-col justify-center align-center">
-                                <button className="px-4 py-2 mb-5 mx-auto bg-gradient-to-r from-indigo-700 to-indigo-500 text-gray-50 rounded-md" type="submit">
-                                    Enregistrer
-                                </button>
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
+             {
+                loading &&
+                <div className="absolute inset-0 z-50 bg-gray-50/30 flex items-center justify-center">
+                    <AiOutlineLoading3Quarters className={`text-6xl text-indigo-500 ${loading && 'animate-spin'}`} />
+                </div>
+            }
+            {
+                !success &&
+                <div className="w-11/12 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white drop-shadow rounded-md p-6">
+                    <h1 className='text-indigo-500 text-center text-3xl mb-3'>Next-Base</h1>
+                    <h2 className='text-gray-500 text-center text-2xl mb-5'>Nouveau mot de passe</h2>
+                    <Formik
+                        initialValues={{
+                            password: '',
+                            confirmPassword: ''
+                        }}
+                        validationSchema={ResetPasswordFormSchema}
+                        onSubmit={handleResetPasswordFormSubmit}
+                    >
+                        {({errors, touched}) => (
+                            <Form>
+                                <div className="flex flex-col mb-5 text-sm relative">
+                                    <label htmlFor="resetPasswordInput" className="text-gray-600 mb-1 ml-1">Mot de passe <span className="text-rose-500">*</span></label>
+                                    <Field name="password" type="password" className="p-2 rounded-lg border-[0.5px] border-gray-200 bg-gray-50" id="resetPasswordInput" placeholder="********" />
+                                    {touched.password && errors.password && <span className='ml-2 flex items-center text-rose-500 absolute bottom-2 right-2'><span className='mr-1'>{errors.password}</span><FiAlertCircle /></span>}
+                                </div>
+                                <div className="flex flex-col mb-5 text-sm relative">
+                                    <label htmlFor="resetConfirmPasswordInput" className="text-gray-600 mb-1 ml-1">Confirmez le mot de passe <span className="text-rose-500">*</span></label>
+                                    <Field name="confirmPassword" type="password" className="p-2 rounded-lg border-[0.5px] border-gray-200 bg-gray-50" id="resetConfirmPasswordInput" placeholder="********" />
+                                    {touched.confirmPassword && errors.confirmPassword && <span className='ml-2 flex items-center text-rose-500 absolute bottom-2 right-2'><span className='mr-1'>{errors.confirmPassword}</span><FiAlertCircle /></span>}
+                                </div>
+                                <div className="flex flex-col justify-center items-center text-sm">
+                                    {error && <p className='text-sm text-red-500 mb-5'>{error}</p>}
+                                    <Button variant={'primary-gradient'} type='submit'>
+                                        <FiSave />
+                                        <span>Enregistrer</span>
+                                    </Button>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+            }
+            {
+                success &&
+                <div className="w-11/12 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white drop-shadow rounded-md p-6">
+                    <h1 className='text-indigo-500 text-center text-3xl mb-3'>Next-Base</h1>
+                    <h2 className='text-gray-500 text-center text-2xl mb-5'>Mot de passe enregistré</h2>
+                    <div className="flex items-center justify-center text-green-500 text-6xl mb-5">
+                        <FiCheckCircle />
+                    </div>
+                    <div className="flex flex-col justify-center items-center text-sm">
+                        <p className="mb-3">Vous allez être redirigé dans quelques secondes</p>
+                        <Button variant={'primary'} href={'/auth/signin'}>
+                            <span>Se connecter</span>
+                            <FiLogIn />
+                        </Button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
